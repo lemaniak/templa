d08858c618e79cc52c43d0b077586e6dcc87e8b6
lemaniak <lema017@gmail.com>
2015-04-22 07:31:06 -0600

    adding more changes

--- templa/app/models/team.rb
+++ templa/app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ActiveRecord::Base
   has_many :players, dependent: :destroy
 
   validates :name, presence: true, length: {minimum: 3, maximum: 100}
-  validates :picture, presence: false , length: {minimum: 5, maximum: 100}
+  validates :picture, presence: false , length: {minimum: 5, maximum: 1000}
   validates_date_of :creation_date, presence: true, before_or_equal_to: Proc.new { Time.now}
 
 
